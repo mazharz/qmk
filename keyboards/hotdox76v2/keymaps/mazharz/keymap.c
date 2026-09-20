94ae2253e43cd7392d914c74f704f19f9ed8e335
Mazhar Zandsalimi <echo.mazhar@gmail.com>
2026-09-20 14:46:11 +0330

    feat(layout): add game layer

--- keyboards/hotdox76v2/keymaps/mazharz/keymap.c
+++ keyboards/hotdox76v2/keymaps/mazharz/keymap.c
@@ -29,7 +29,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌──────┬───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┬──────┐
-     * │      │ 1 │ 2 │ 3 │ 4 │ 5 │   │       │   │ 6 │ 7 │ 8 │ 9 │ 0 │      │
+     * │      │ 1 │ 2 │ 3 │ 4 │ 5 │   │       │   │ 6 │ 7 │ 8 │ 9 │ 0 │  GME │
      * ├──────┼───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┼──────┤
      * │ Tab  │ q │ w │ e │ r │ t │   │       │   │ y │ u │ i │ o │ p │    \ │
      * ├──────┼───┼───┼───┼───┼───┤   │       │   ├───┼───┼───┼───┼───┼──────┤
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                    └───┴───┴───┘       └───┴───┴───┘
      */
     [0] = LAYOUT_ergodox_pretty(
-        KC_NO,   KC_1,   KC_2,   KC_3,    KC_4,          KC_5,   KC_NO,       KC_NO, KC_6,    KC_7,         KC_8,    KC_9,   KC_0,    KC_NO,
+        KC_NO,   KC_1,   KC_2,   KC_3,    KC_4,          KC_5,   KC_NO,       KC_NO, KC_6,    KC_7,         KC_8,    KC_9,   KC_0,    TG(3),
         KC_TAB,  KC_Q,   KC_W,   KC_E,    KC_R,          KC_T,   KC_NO,       KC_NO, KC_Y,    KC_U,         KC_I,    KC_O,   KC_P,    KC_BSLS,
         KC_ESC,  HMT_CA, HMT_SS, HMT_GD,  HMT_AF,        KC_G,                       KC_H,    HMT_AJ,       HMT_GK,  HMT_SL, HMT_CS,  KC_QUOT,
         KC_LSFT, KC_Z,   KC_X,   KC_C,    KC_V,          KC_B,   KC_NO,       KC_NO, KC_N,    KC_M,         KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
@@ -116,5 +116,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                        KC_NO, KC_NO,       KC_NO, KC_NO,
                                                               KC_NO,       KC_NO,
                                                 KC_NO, KC_NO, KC_NO,       KC_NO, KC_NO, KC_NO
+    ),
+    /*
+     * game mode - toggled on and off by the top right 1.5u key (GME on layer 0).
+     * same as layer 0 without the home row mods; real modifiers get their own
+     * keys instead - shift stays left of z, ctrl sits below it, alt sits left
+     * of the standalone gui. this layer sits above layers 1 and 2 and is fully
+     * populated, so those layers are unreachable while it is on - space and the
+     * thumb keys are plain keycodes here rather than layer taps.
+     *
+     * ┌──────┬───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┬──────┐
+     * │      │ 1 │ 2 │ 3 │ 4 │ 5 │   │       │   │ 6 │ 7 │ 8 │ 9 │ 0 │  GME │
+     * ├──────┼───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┼──────┤
+     * │ Tab  │ q │ w │ e │ r │ t │   │       │   │ y │ u │ i │ o │ p │    \ │
+     * ├──────┼───┼───┼───┼───┼───┤   │       │   ├───┼───┼───┼───┼───┼──────┤
+     * │Escape│ a │ s │ d │ f │ g ├───┤       ├───┤ h │ j │ k │ l │ ; │    ' │
+     * ├──────┼───┼───┼───┼───┼───┤   │       │   ├───┼───┼───┼───┼───┼──────┤
+     * │Shift │ z │ x │ c │ v │ b │   │       │   │ n │ m │ , │ . │ / │ Shift│
+     * └──┬───┼───┼───┼───┼───┼───┴───┘       └───┴───┼───┼───┼───┼───┼───┬──┘
+     *    │Ctl│   │Alt│ G │Spc│                       │Spc│ G │   │   │   │
+     *    └───┴───┴───┴───┴───┘                       └───┴───┴───┴───┴───┘
+     *                        ┌───┬───┐       ┌───┬───┐
+     *                        │   │   │       │   │   │
+     *                    ┌───┼───┼───┤       ├───┼───┼───┐
+     *                    │   │   │   │       │   │   │   │
+     *                    │BSP│DEL├───┤       ├───┤CAP│ENT│
+     *                    │   │   │   │       │   │   │   │
+     *                    └───┴───┴───┘       └───┴───┴───┘
+     */
+    [3] = LAYOUT_ergodox_pretty(
+        KC_NO,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_NO,       KC_NO, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    TG(3),
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_NO,       KC_NO, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_NO,       KC_NO, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        KC_LCTL, KC_NO,   KC_LALT, KC_LGUI, KC_SPC,                               KC_SPC,  KC_LGUI, KC_NO,   KC_NO,   KC_NO,
+                                                     KC_NO,   KC_NO,       KC_NO, KC_NO,
+                                                              KC_NO,       KC_NO,
+                                            KC_BSPC, KC_DEL,  KC_NO,       KC_NO, KC_CAPS, KC_ENT
     )
 };
